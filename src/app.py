973df8e936bdfd6cb8dd7f9fdce045f0b1c6a2b1
Simone Pandolfi <simopandolfi@gmail.com>
2017-01-04 16:37:33 +0100

    test_11

--- src/app.py
+++ src/app.py
@@ -40,37 +40,10 @@ logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
 
 @ask.intent('SayHelloIntent', convert={'name': str})
-def say_hello(name: str) -> str:
+def say_hello(name: str):
     text = "Hello, {0}!".format(name)
     return statement('<speak>{0}</speak>'.format(text))
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# @ask.launch
-# def launch():
-#     speech_text = 'Welcome to the Alexa Skills Kit, you can say hello'
-#     return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
-#
-#
-# @ask.intent('HelloWorldIntent')
-# def hello_world():
-#     speech_text = 'Hello world'
-#     return statement(speech_text).simple_card('HelloWorld', speech_text)
-#
-#
-# @ask.intent('AMAZON.HelpIntent')
-# def help():
-#     speech_text = 'You can say hello to me!'
-#     return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
-#
-#
-# @ask.session_ended
-# def session_ended():
-#     return "", 200
-#
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
